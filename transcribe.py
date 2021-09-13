c85eac6de93e55ca828d382f524a05a909ed07e4
jmska <ljrepa@gmail.com>
2021-09-12 21:16:21 -0400

    Second of silence at beginning of every audio file to prevent weird OBS browser source stuttering bug. Relative paths!!

--- transcribe.py
+++ transcribe.py
@@ -1,7 +1,7 @@
 # robojamieMacBottald / transcribe.py
 # Python program to generate audiokey.json used to match BYSB dialogue files 
 # to the transcription of the dialogue and the kid saying it.
-# Requires speech_recognition and pydub
+# Requires speech_recognition, pydub, pathlib and librosa
 
 import speech_recognition as sr
 import os
@@ -10,7 +10,7 @@ import json
 import librosa
 from pathlib import Path
 
-root_dir = Path('D:\\dbots\\robojamie\\audio')
+root_dir = Path('./audio')
 
 file_list = [f for f in root_dir.glob('**/*') if f.is_file()]
 
@@ -28,16 +28,26 @@ with open("kids.json") as kidsf:
     kids.append(json.load(kidsf))
 
 for file in file_list:
+
     f = str(file)
+    f_pad = os.path.splitext(f)[0] + "_pad.mp3"
+    pad = AudioSegment.silent(duration = 1000)
 
     if (f.endswith('.mp3')):
         sound = AudioSegment.from_mp3(f)
+        sound = sound + pad
         sound.export("transcript.wav", format="wav")
+        sound.export(f_pad, format="mp3")
+        
     elif (f.endswith('.wav')):
         sound = AudioSegment.from_wav(f)
+        sound = sound + pad
         sound.export("transcript.wav", format="wav")
+        sound.export(f_pad, format="mp3")
     else:
-        continue   
+        continue
+    
+
 
     AUDIO_FILE = "transcript.wav"
                         
@@ -77,9 +87,9 @@ for file in file_list:
         except:
             transcription = ""
         
-        data = {"filename": f, "kid": kid_name, "duration": duration,  "transcription": transcription}
+        data = {"filename": f_pad, "kid": kid_name, "duration": duration,  "transcription": transcription}
         print(data)
-        if (len(transcription) > 0 or duration > 1.0):
+        if (len(transcription) > 0 or duration > 2.0):
 
             if(kid_game == 'bysb'):
                 bysb.append(data)
@@ -99,20 +109,20 @@ for file in file_list:
             elif(kid_game == 'rata'):
                 rata.append(data)
 
-with open("D:\\dbots\\robojamie\\audio_keys\\bysb.json", "a+") as out:
+with open("./audio_keys/bysb.json", "w") as out:
     json.dump(bysb, out)
 
-with open("D:\\dbots\\robojamie\\audio_keys\\byba.json", "a+") as out:
+with open("./audio_keys/byba.json", "w+") as out:
     json.dump(byba, out)
 
-with open("D:\\dbots\\robojamie\\audio_keys\\cars.json", "a+") as out:
+with open("./audio_keys/cars.json", "w+") as out:
     json.dump(cars, out)
 
-with open("D:\\dbots\\robojamie\\audio_keys\\degs.json", "a+") as out:
+with open("./audio_keys/degs.json", "w+") as out:
     json.dump(degs, out)
 
-with open("D:\\dbots\\robojamie\\audio_keys\mgtt.json", "a+") as out:
+with open("./audio_keys/mgtt.json", "w+") as out:
     json.dump(mgtt, out)
 
-with open("D:\\dbots\\robojamie\\audio_keys\\rata.json", "a+") as out:
-    json.dump(rata, out)    
+with open("./audio_keys/rata.json", "w+") as out:
+    json.dump(rata, out)
